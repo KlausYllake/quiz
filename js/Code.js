08b18b9d0475fc4f4298de4e95c202dccb8503b3
KlausYllake <88346056+KlausYllake@users.noreply.github.com>
2022-06-08 18:58:38 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 80);
+
+  // Adicione código para exibir as outras duas perguntas.
+  text("Dica: executa uma tarefa específica!",700,180);
+  text("Dica: Armazena informação!",100,300);
+
+  textSize(30);
+  text("VERÁLABI",100,60);
+  text("BED CANDO SODA",100,280);
+  text("NOÇUFÃ",700,160);
+}
